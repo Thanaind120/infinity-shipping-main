c4f946fd1b2a906d29ea816a1a49c46dbf4feede
Thanai Likitpoonsup <thanai.ots2565@gmail.com>
2022-11-23 10:39:26 +0700

    Menu Updated

--- resources/views/include/menu.blade.php
+++ resources/views/include/menu.blade.php
@@ -9,7 +9,7 @@
         <ul class="sidebar-menu">
             <li class="menu-header">Home</li>
             <li class="<?php echo (isset($active[0]))?$active[0]:'';?>"><a class="nav-link" href="{{ url('/backend/home/banner') }}"><i class="fas fa-bullhorn"></i> <span>Slide Banner</span></a></li>
-            <li class="<?php echo (isset($active[1]))?$active[1]:'';?>"><a class="nav-link" href="{{ url('/backend/home/logistics-service') }}"><i class="fas fa-wrench"></i> <span>Logistics Service</span></a></li>
+            <li class="<?php echo (isset($active[1]))?$active[1]:'';?>"><a class="nav-link" href="{{ url('/backend/home/logistics-service-topics') }}"><i class="fas fa-wrench"></i> <span>Logistics Service Topics</span></a></li>
             
             <li class="menu-header">inventory</li>
             <li><a class="nav-link" href="/"><i class="fas fa-users"></i> <span>ซัพพลายเออร์</span></a></li>
