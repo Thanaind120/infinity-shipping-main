d5e1a8646d2a4952087519ac679c219abd58f2f1
Thanaind120 <97428470+Thanaind120@users.noreply.github.com>
2023-01-30 16:33:01 +0700

    Updated

--- resources/views/include/nav.blade.php
+++ resources/views/include/nav.blade.php
@@ -12,11 +12,11 @@
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image" src="{{ asset('backend/assets/img/avatar/avatar-1.png') }}"
                     class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }} !</div>
+                <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::guard('web')->user()->name }} !</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-title">Logged in</div>
-                <a href="features-profile.html" class="dropdown-item has-icon">
+                <a href="{{ url('backend/user-management/edit/'.Auth::guard('web')->user()->id) }}" class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>
                 {{-- <a href="features-activities.html" class="dropdown-item has-icon">
